a2a9e0afc0990788482d8a95e00862a12aa741f4
Redaite <redaitekalgeteye@gmail.com>
2022-08-24 15:32:20 +0300

    task 10

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,4 @@
+print("The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.\nFlat is better than nested.\n
+Sparse is better than dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.\nAlthough practicality beats purity.\n
+Errors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferablyonly one --obvious way to do it\n.Although that way may not be obvious at first unless you're Dutch.\nNow is better than never.\nAlthough never is often better than *right* now.\nIf the implementation is hard to explain, it's a bad idea.\n
+If the implementation is easy to explain, it may be a good idea.\nNamespaces areone honking great idea -- let's do more of those!")

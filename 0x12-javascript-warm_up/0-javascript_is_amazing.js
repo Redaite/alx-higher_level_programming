a750fcc33b300f98932b46ef3d170cae73d68cb7
Redaite <104760480+Redaite@users.noreply.github.com>
2023-11-18 10:49:48 +0300

    Update 0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
+// 0-javascript_is_amazing.js
 
-// Script that prints "JavaScript is amazing".
-
-const myVar = 'JavaScript is amazing';
+const myVar = "JavaScript is amazing";
 console.log(myVar);

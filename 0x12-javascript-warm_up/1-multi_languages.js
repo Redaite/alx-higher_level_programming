f7361f1f4f6f3de0ca0188f57c00a973ebfcc2a1
Redaite <104760480+Redaite@users.noreply.github.com>
2023-11-18 10:51:32 +0300

    Update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-// Script that prints "JavaScript is amazing".
+// 3-lines.js
 
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
